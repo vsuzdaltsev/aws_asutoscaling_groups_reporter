3a630f0eec795faf984a578ebd1382a23a65a674
Vsevolod Suzdaltsev <vsevolod.suzdaltsev@spanning.com>
2019-08-18 14:09:00 +0300

    add sleeping asgs to report as a separate namespace

--- asg_report.rb
+++ asg_report.rb
@@ -7,7 +7,11 @@ require_relative './lib/constants'
 require_relative './lib/aws_asgs'
 require_relative './lib/aws_regions'
 
+ENVIRONMENTS = Aws::Constants::ENVIRONMENTS
+ASGS_TYPES   = Aws::Constants::ASGS_TYPES
+
 # This may be deployed as an aws lambda
+# Arguments needed if run from aws lambda
 def auto_scaling_groups(*)
   regions = Aws::Regions.list_used
   json    = JSON.dump(all_asgs(regions))
@@ -17,9 +21,14 @@ def auto_scaling_groups(*)
   { statusCode: 200, body: json }
 end
 
-def environment_asgs(env, regions)
+# @param env [String] - set within 'environment' tag
+# @param regions [Array] - aws regions to lookup
+# @param asgs_type [String] - 'running_asgs' or 'sleeping_asgs'.
+#   Running asg has at least one EC2 running or desired
+# @return [Hash]
+def environment_asgs(env, regions, asgs_type)
   value = regions.map do |region|
-    { region => Aws::Asgs.new(region: region, filter_by_tags: { environment: env }).running_asgs }
+    { region => Aws::Asgs.new(region: region, filter_by_tags: { environment: env }).send(asgs_type) }
   end
 
   { env => value }
@@ -27,8 +36,10 @@ end
 
 def all_asgs(regions)
   {}.tap do |asgs|
-    Aws::Constants::ENVIRONMENTS.each do |env|
-      asgs.merge!(environment_asgs(env, regions))
+    ENVIRONMENTS.each do |env|
+      ASGS_TYPES.each do |asgs_type|
+        asgs[asgs_type] = environment_asgs(env, regions, asgs_type)
+      end
     end
   end
 end
--- lib/constants.rb
+++ lib/constants.rb
@@ -2,7 +2,8 @@
 
 module Aws
   module Constants
-    USED_REGIONS = %w[us-east-1 us-west-1].freeze
+    USED_REGIONS = %w[us-east-1].freeze
     ENVIRONMENTS = %w[dev stage prod].freeze
+    ASGS_TYPES   = %w[running_asgs sleeping_asgs].freeze
   end
 end
